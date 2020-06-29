4614f811934231dc8d7800fcab5e1c5b5ba92435
Nebulis <nebounet@gmail.com>
2020-06-29 13:29:23 +0800

    fix: code review

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@
 import { Context, Callback } from "aws-lambda";
 import { Route53 } from "aws-sdk";
 import { uniqueNamesGenerator, adjectives, colors, animals } from "unique-names-generator";
-import { getExecutionName } from "./getExecutionName";
 
 const domain = process.env.DOMAIN;
 const hostedZoneId = process.env.HOSTED_ZONE_ID || "";
@@ -53,26 +52,26 @@ const createRecord = ({
   };
 };
 
-module.exports = {
-  create: (event: CreateEvent, context: Context, callback: Callback<CleanEvent>) => {
-    console.log(event);
+export const create = (event: CreateEvent, context: Context, callback: Callback<CleanEvent>): void => {
+  console.log(event);
 
-    if (!event.address) throw new Error("Please provide an address");
-    if (!event.networkId || !isFinite(event.networkId)) throw new Error("Please provide a networkId that is a number");
-    const name = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals], separator: "-" });
+  if (!event.address) throw new Error("Please provide an ethereum address");
+  if (!event.networkId || !isFinite(event.networkId)) throw new Error("Please provide a networkId that is a number");
+  const name = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals], separator: "-" });
 
-    route53.changeResourceRecordSets(createRecord({ action: "CREATE", ...event, name }), (err) => {
-      callback(err, { name, ...event });
-    });
-  },
-  clean: (event: CleanEvent, context: Context, callback: Callback<CleanEvent>) => {
-    console.log(event);
-    if (!event.address) throw new Error("Please provide an address");
-    if (!event.networkId || !isFinite(event.networkId)) throw new Error("Please provide a networkId that is a number");
-    if (!event.name) throw new Error("Please provide a name");
-    route53.changeResourceRecordSets(createRecord({ action: "DELETE", ...event }), (err) => {
-      callback(err, event);
-    });
-  },
-  getExecutionName,
+  route53.changeResourceRecordSets(createRecord({ action: "CREATE", ...event, name }), (err) => {
+    callback(err, { name, ...event });
+  });
 };
+
+export const clean = (event: CleanEvent, context: Context, callback: Callback<CleanEvent>): void => {
+  console.log(event);
+  if (!event.address) throw new Error("Please provide an ethereum address");
+  if (!event.networkId || !isFinite(event.networkId)) throw new Error("Please provide a networkId that is a number");
+  if (!event.name) throw new Error("Please provide a name");
+  route53.changeResourceRecordSets(createRecord({ action: "DELETE", ...event }), (err) => {
+    callback(err, event);
+  });
+};
+
+export { getExecutionDetails } from "./getExecutionDetails";
