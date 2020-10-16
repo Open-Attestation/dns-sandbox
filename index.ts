014054324fad46c41e5188bf0fefb9bb023b07a9
Nebulis <nebounet@gmail.com>
2020-10-16 17:29:18 +0800

    feat: support public key records

--- index.ts
+++ index.ts
@@ -8,27 +8,29 @@ const domain = process.env.DOMAIN;
 const hostedZoneId = process.env.HOSTED_ZONE_ID || "";
 const route53 = new Route53();
 
-interface CreateEvent {
+interface DocumentStoreEvent {
   address: string;
   networkId: number;
 }
+interface PublicKeyEvent {
+  algorithm: string;
+  publicKey: string;
+}
+type CreateEvent = Partial<DocumentStoreEvent> & Partial<PublicKeyEvent>;
 
-interface CleanEvent {
-  address: string;
-  networkId: number;
-  name: string;
-}
+type CleanEvent = Partial<DocumentStoreEvent> &
+  Partial<PublicKeyEvent> & {
+    name: string;
+  };
 
 const createRecord = ({
   action,
-  address,
   name,
-  networkId,
+  value,
 }: {
-  address: string;
-  networkId: number;
   name: string;
   action: string;
+  value: string;
 }): Route53.Types.ChangeResourceRecordSetsRequest => {
   return {
     ChangeBatch: {
@@ -39,7 +41,7 @@ const createRecord = ({
             Name: `${name}.${domain}`,
             ResourceRecords: [
               {
-                Value: `"openatts net=ethereum netId=${networkId} addr=${address}"`,
+                Value: value,
               },
             ],
             TTL: 60,
@@ -52,24 +54,44 @@ const createRecord = ({
   };
 };
 
+//
+const isEthereumAddressEvent = (event: CreateEvent): event is DocumentStoreEvent =>
+  !!event.address && !!event.networkId && isFinite(event.networkId);
+const createEthereumAddressRecord = ({ networkId, address }: DocumentStoreEvent): string =>
+  `"openatts net=ethereum netId=${networkId} addr=${address}"`;
+
+const isPublicKeyEvent = (event: CreateEvent): event is PublicKeyEvent => !!event.algorithm && !!event.publicKey;
+const createPublicKeyRecord = ({ algorithm, publicKey }: PublicKeyEvent): string =>
+  `"openatts a=${algorithm}; p=${publicKey}; v=1.0;"`;
+
 export const create = (event: CreateEvent, context: Context, callback: Callback<CleanEvent>): void => {
   console.log(event);
 
-  if (!event.address) throw new Error("Please provide an ethereum address");
-  if (!event.networkId || !isFinite(event.networkId)) throw new Error("Please provide a networkId that is a number");
+  let value = "";
+  if (isEthereumAddressEvent(event)) value = createEthereumAddressRecord({ ...event });
+  else if (isPublicKeyEvent(event)) value = createPublicKeyRecord({ ...event });
+  else
+    throw new Error(
+      "Please provide information for document store (networkId and address) or information for publicKey (algorithm and publicKey)"
+    );
   const name = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals], separator: "-" });
 
-  route53.changeResourceRecordSets(createRecord({ action: "CREATE", ...event, name }), (err) => {
+  route53.changeResourceRecordSets(createRecord({ action: "CREATE", value, name }), (err) => {
     callback(err, { name, ...event });
   });
 };
 
 export const clean = (event: CleanEvent, context: Context, callback: Callback<CleanEvent>): void => {
   console.log(event);
-  if (!event.address) throw new Error("Please provide an ethereum address");
-  if (!event.networkId || !isFinite(event.networkId)) throw new Error("Please provide a networkId that is a number");
+  let value = "";
+  if (isEthereumAddressEvent(event)) value = createEthereumAddressRecord({ ...event });
+  else if (isPublicKeyEvent(event)) value = createPublicKeyRecord({ ...event });
+  else
+    throw new Error(
+      "Please provide information for document store (networkId and address) or information for publicKey (algorithm and publicKey)"
+    );
   if (!event.name) throw new Error("Please provide a name");
-  route53.changeResourceRecordSets(createRecord({ action: "DELETE", ...event }), (err) => {
+  route53.changeResourceRecordSets(createRecord({ action: "DELETE", value, name: event.name }), (err) => {
     callback(err, event);
   });
 };
